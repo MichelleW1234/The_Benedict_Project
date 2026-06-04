f43be0c5d59477989ee6b41185d782efc5809ce2
tag717 <97643647+tag717@users.noreply.github.com>
2026-06-03 19:50:18 -0700

    BUNCH

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections; // Required for Coroutines
 //using System.Diagnostics;
 
 public class AgentController : MonoBehaviour
@@ -6,14 +7,26 @@ public class AgentController : MonoBehaviour
     public Animator animator;
     public float sampleRadius = 1.0f;
 
+    [SerializeField] 
+    private Transform rightHand;
+    [SerializeField]
+    private GameObject flowerPrefab;
+
+    private GameObject spawnedFlower;
+    private bool FlowerPlaying = false;
+
     void Start()
     {
         if (animator != null) {
             // call gesture functions here for testing
+            PlayFlowerGesture();
         }
     }
     void Update()
     {
+        if(spawnedFlower != null){
+            Destroy(spawnedFlower, 5);
+        }
         
     }
 
@@ -54,10 +67,33 @@ public class AgentController : MonoBehaviour
         animator.SetBool("Talking_on_phone", false);
     }
 
-     void PlayFlowerGesture() {
-        animator.SetTrigger("Flower");
+    void PlayFlowerGesture() {
+        FlowerPlaying = true;
+        StartCoroutine(FlowerSequence());
+        FlowerPlaying = false;
     }
 
+    private IEnumerator FlowerSequence() {
+        animator.SetTrigger("Flower");
+        CreateFlower();
 
+        yield return null;
+
+        float animationLength = animator.GetCurrentAnimatorStateInfo(0).length;
+
+        yield return new WaitForSeconds(animationLength + 3.0f);
+
+        
+    }
+
+    void CreateFlower() {
+        if (rightHand != null && flowerPrefab != null) 
+        {
+            // Instantiate the flower and make it a child of the rightHand
+            spawnedFlower = Instantiate(flowerPrefab, rightHand);
+            spawnedFlower.transform.localPosition = Vector3.zero;
+            spawnedFlower.transform.localScale = Vector3.one * 80f;
+        }
+    }
 
 }
